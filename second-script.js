0f56bea08440ff2371fa09aebf96d1876611578b
Suffer Engineer <57523413+hubilya@users.noreply.github.com>
2024-08-13 20:41:22 +0300

    Update second-script.js

--- second-script.js
+++ second-script.js
@@ -1,1 +1,10 @@
 
+function myFunction() {
+        var x = document.getElementById("myNav");
+            if (x.className === "nav") {
+            x.className += " responsive";
+            } 
+            else {
+                x.className = "nav";
+            }
+     }
